b17db699eaf421aa492b8fc5f80af36ec0212c97
栗原佳輝 <yosyos@kuriharakeiakiranoMacBook-Pro.local>
2017-12-12 17:43:03 +0900

    completed for now

--- player.py
+++ player.py
@@ -2,38 +2,46 @@
 # -*- coding: utf-8 -*-
 
 import bullet
-import player_const
+from player_const import *
+import random
+import math
+# for debug
+import time
 
 class Player:
     def __init__(self, x, y):
-        self.size = player_constPLAYER_SIZE
-        self.vx=10
-        self.vy=10
+        self.size = PLAYER_SIZE
+        self.x = x
+        self.y = y
 
     # Playerの動き制御
-    def moving(self):
-        vx1 = self.ax+self.vx
-        vy1 = self.ay+self.vy
-        dx = (self.vx+vx1)/2
-        dy = (self.vy+vy1)/2
-        self.vx=vx1
-        self.vy=vy1
-        # 次の速度をランダムに
-        self.ax = R.randint(-10,10) - self.vx/2
-        self.ay = R.randint(-10,10) - self.vy/2
+    def move(self):
+        radian = math.radians(random.randint(-180,180))
+        vx = math.sin(radian) * PLAYER_VELOCITY 
+        vy = math.cos(radian) * PLAYER_VELOCITY
+        self.x += vx
+        self.y += vy
         # 壁に当たると反射する処理
+        if self.x < 0 :
+            self.x = abs(self.x)
+        elif self.x >  WIDTH:
+            self.x = WIDTH - self.x % WIDTH
+        if self.y < 0 :
+            self.y = abs(self.y)
+        elif self.y >  HEIGHT:
+            self.y = HEIGHT - self.y % HEIGHT
 
     # 銃弾の発射
     def shot(self,radian):
         # bulletインスタンスの生成
         ## radianに沿ってbulletのインスタンスを生成する
-        bullet = bullet(x,y,size,radian,v,damege)
+        bullet = bullet(self.x,self.y,BULLET_SIZE,radian,BULLET_VELOCITY,BULLET_DAMAGE)
         return bullet
 
 if __name__ == '__main__':
     player = Player(250,250)
     while True:
+        player.move()
+        print(player.x,player.y)
+        time.sleep(1)
         
-        
-        
-    
