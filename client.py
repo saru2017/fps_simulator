8be32d816d051260db714e6669c9d324559066a1
yusk <tdfagamdb186@yahoo.co.jp>
2018-01-23 14:17:37 +0900

    wip: clientっぽいものの実装

---
+++ client.py
@@ -0,0 +1,106 @@
+import random
+
+
+class Client():
+    def __init__(self, frame, player, enemy, enemy_latency):
+        self.frame = frame
+        self.player = player
+        self.enemy = enemy
+        self.enemy_latency = enemy_latency
+
+        self.bullets = []
+        self.event_queue = []
+
+    def __set_object(self, object):
+        x1 = object.x
+        y1 = object.y
+        x2 = object.x + object.size
+        y2 = object.y + object.size
+        self.frame.cvs.coords(object.id, x1, y1, x2, y2)
+
+    def __reserve_event(self, event_name, count):
+        self.event_queue.append({
+            'name': event_name,
+            'count': count
+        })
+
+    def move(self):
+        self.player.move()
+        self.__set_object(self.player)
+
+    def shot(self):
+        bullet = self.player.shot(self.frame, random.randint(-180, 180))
+        self.__set_object(bullet)
+        self.bullets.append(bullet)
+
+    def enemy_move(self):
+        self.enemy.move()
+        self.__set_object(self.enemy)
+
+    def enemy_shot(self):
+        bullet = self.enemy.shot(self.frame, random.randint(-180, 180))
+        self.__set_object(bullet)
+        self.bullets.append(bullet)
+
+    def bullet_move(self):
+        for bullet in self.bullets:
+            bullet.move()
+            self.__set_object(bullet)
+
+    def reserve_move(self):
+        self.__reserve_event('move', 0)
+
+    def reserve_shot(self):
+        self.__reserve_event('shot', 0)
+
+    def reserve_enemy_move(self):
+        self.__reserve_event('enemy_move', self.enemy_latency)
+
+    def reserve_enemy_shot(self):
+        self.__reserve_event('enemy_shot', self.enemy_latency)
+
+    def __do_event(self):
+        # 実行
+        done_event_indexes = []
+        for i, event in enumerate(self.event_queue):
+            if event['count'] <= 0:
+                getattr(self, event['name'])()
+                done_event_indexes.append(i)
+        # 削除
+        for index in sorted(done_event_indexes, reverse=True):
+            del self.event_queue[index]
+        # 進む
+        for i in range(len(self.event_queue)):
+            self.event_queue[i]['count'] -= 1
+
+    def update(self):
+        # 予約
+        self.reserve_move()
+        self.reserve_shot()
+        self.reserve_enemy_move()
+        self.reserve_enemy_shot()
+        # 実行
+        self.__do_event()
+
+
+if __name__ == '__main__':
+    import tkinter as Tk
+    from frame import Frame
+    from player import Player
+    frame = Frame()
+    player = Player.create(frame, 0, 0)
+    enemy = Player.create(frame, 0, 0)
+    ENEMY_LATENCY = 3
+    client = Client(frame, player, enemy, ENEMY_LATENCY)
+
+    def update():
+        client.update()
+        # print("player: (%.1f, %.1f), enemy: (%.1f, %.1f)" % (
+        #     client.player.x, client.player.y,
+        #     client.enemy.x, client.enemy.y))
+        print(client.event_queue)
+        frame.cvs.after(100, update)
+
+    update()
+    frame.pack(fill=Tk.BOTH, expand=1)
+    frame.mainloop()
--- frame.py
+++ frame.py
@@ -1,32 +1,6 @@
 import tkinter as Tk
-import random as R
 
 BACKGROUND_COLOR = 'white'
-PLAYER_SIZE = 10
-PLAYER_COLOR = 'red'
-
-
-class Player:
-    def __init__(self, x, y, canvas):
-        self.canvas = canvas
-        self.id = self.canvas.create_oval(x - PLAYER_SIZE, y - PLAYER_SIZE, x + PLAYER_SIZE, y + PLAYER_SIZE, fill=PLAYER_COLOR, width=0)
-        self.vx = 0
-        self.vy = 0
-        self.ax = R.randint(-10, 10)
-        self.ay = R.randint(-10, 10)
-
-    # Playerの動き制御
-    def moving(self):
-        vx1 = self.ax + self.vx
-        vy1 = self.ay + self.vy
-        dx = (self.vx + vx1) / 2
-        dy = (self.vy + vy1) / 2
-        self.canvas.move(self.id, dx, dy)
-        self.vx = vx1
-        self.vy = vy1
-        self.ax = R.randint(-10, 10) - self.vx / 2
-        self.ay = R.randint(-10, 10) - self.vy / 2
-        print(self.ax, self.ay)
 
 
 class Frame(Tk.Frame):
@@ -36,15 +10,3 @@ class Frame(Tk.Frame):
         self.master.geometry("+20+20")
         self.cvs = Tk.Canvas(self, width=500, height=500, relief=Tk.SUNKEN, borderwidth=2, bg=BACKGROUND_COLOR)
         self.cvs.pack(fill=Tk.BOTH, expand=1)
-        # x = R.randint(50, 450)
-        # y = R.randint(50, 450)
-        # self.playerA = Player(x, y, self.cvs)
-        # x = R.randint(50, 450)
-        # y = R.randint(50, 450)
-        # self.playerB = Player(x, y, self.cvs)
-        # self.move_player()
-
-    def move_player(self):
-        self.playerA.moving()
-        self.playerB.moving()
-        self.cvs.after(100, self.move_player)
